2732fa021bc74fd343ba8a03cf4997cb255743ac
pfuchs <email@patrick-fuchs.com>
2022-04-20 13:40:58 +0200

    fix rra rrb rrr

--- pain.sh
+++ pain.sh
@@ -3,9 +3,9 @@ NBR_COUNT=10
 NBR=$(seq 1 $NBR_COUNT | shuf | tr '\n' ' ' | head -c -1)
 CMD=$( ./push_swap $NBR)
 NBR_LINK=$( echo "$NBR" | tr ' ' ',' )
-CMD_LINK=$( echo "$CMD" | sed -e"s/sa/a/g" -e"s/sb/b/g" -e"s/ss/c/g" \
--e"s/ra/d/g" -e"s/rb/e/g" -e"s/rr/f/g" -e"s/rra/g/g" -e"s/rrb/h/g" \
--e"s/rrr/i/g" -e"s/pa/j/g" -e"s/pb/k/g" | tr -d '\n')
+CMD_LINK=$( echo "$CMD" | sed -e"s/rra/g/g" -e"s/rrb/h/g" \
+-e"s/rrr/i/g" -e"s/sa/a/g" -e"s/sb/b/g" -e"s/ss/c/g" \
+-e"s/ra/d/g" -e"s/rb/e/g" -e"s/rr/f/g"  -e"s/pa/j/g" -e"s/pb/k/g" | tr -d '\n')
 CMD_COUNT=${#CMD_LINK}
 echo "Operations: $CMD_COUNT"
 echo "https://bluegales.github.io/push-swap-pain/?&nbr=$NBR_LINK&cmd=$CMD_LINK"
